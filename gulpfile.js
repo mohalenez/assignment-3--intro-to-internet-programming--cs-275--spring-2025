a921fd7373b82848d5e71c511c05b1d246e79589
alenezi <m.alenezi.ku@gmail.com>
2025-04-09 23:19:19 +0500

    cnages

---
+++ gulpfile.js
@@ -0,0 +1,61 @@
+const gulp = require("gulp");
+const eslint = require("gulp-eslint");
+const stylelint = require("gulp-stylelint");
+const babel = require("gulp-babel");
+const browserSync = require("browser-sync").create();
+const postcss = require("gulp-postcss");
+const cssnano = require("cssnano");
+const uglify = require("gulp-uglify");
+const { src, dest, parallel, series } = require("gulp");
+
+function lintCSS() {
+    return src("styles/*.css").pipe(
+        stylelint({
+            configFile: ".stylelintrc.json",
+            reporters: [{ formatter: "string", console: true }],
+        })
+    );
+}
+
+function lintJS() {
+    return src("scripts/*.js")
+        .pipe(eslint({ useEslintrc: true }))
+        .pipe(eslint.format());
+}
+
+function transpileJS() {
+    return src("scripts/main.js")
+        .pipe(babel({ presets: ["@babel/preset-env"] }))
+        .pipe(dest("dist/"));
+}
+
+function serve() {
+    browserSync.init({ server: "./" });
+    gulp.watch("styles/*.css", gulp.series(lintCSS, reload));
+    gulp.watch("scripts/*.js", gulp.series(lintJS, transpileJS, reload));
+}
+
+function reload(done) {
+    browserSync.reload();
+    done();
+}
+
+function buildCSS() {
+    return src("styles/main.css")
+        .pipe(postcss([cssnano()]))
+        .pipe(dest("prod/styles"));
+}
+
+function buildJS() {
+    return src("scripts/main.js")
+        .pipe(babel({ presets: ["@babel/preset-env"] }))
+        .pipe(uglify())
+        .pipe(dest("prod/scripts"));
+}
+
+function copyHTML() {
+    return src("index.html").pipe(dest("prod"));
+}
+
+exports.default = series(parallel(lintCSS, lintJS), transpileJS, serve);
+exports.build = series(parallel(buildCSS, buildJS), copyHTML);
---
+++ scripts/main.js
@@ -0,0 +1,59 @@
+const triggers = {
+    menu: document.querySelector(`#js-triggers li:first-child a`),
+    modal: document.querySelector(`#js-triggers li:nth-child(2) a`),
+};
+
+const components = {
+    menu: document.querySelector(`nav > ul`),
+    modal: document.querySelector(`.modal-panel`),
+    content: document.querySelector(`.modal-content`),
+};
+
+let isMenuOpen = false;
+let isModalOpen = false;
+
+const toggleMenu = (e) => {
+    e.preventDefault();
+    isMenuOpen = !isMenuOpen;
+    components.menu.classList.toggle(`menu-active`, isMenuOpen);
+};
+
+const toggleModal = (e) => {
+    e.preventDefault();
+    isModalOpen = !isModalOpen;
+    components.modal.classList.toggle(`active`, isModalOpen);
+
+    if (isModalOpen) {
+        components.content.innerHTML = `<h2>Important Notice</h2>
+            <p>This modal should obscure all other page elements.</p>`;
+        document.body.style.overflow = `hidden`;
+    } else {
+        document.body.style.overflow = ``;
+    }
+};
+
+const handleResize = () => {
+    const wasMenuOpen = isMenuOpen;
+    const wasModalOpen = isModalOpen;
+
+    if (wasMenuOpen || wasModalOpen) {
+        components.menu.classList.remove(`menu-active`);
+        components.modal.classList.remove(`active`);
+        isMenuOpen = false;
+        isModalOpen = false;
+        document.body.style.overflow = ``;
+    }
+};
+
+const handleKeyPress = (e) => {
+    if (e.key === `Escape` && isModalOpen) toggleModal(e);
+};
+
+triggers.menu.addEventListener(`click`, toggleMenu);
+triggers.modal.addEventListener(`click`, toggleModal);
+window.addEventListener(`resize`, handleResize);
+document.addEventListener(`keydown`, handleKeyPress);
+
+components.modal.addEventListener(`click`, (e) => {
+    if (e.target === components.modal) toggleModal(e);
+});
